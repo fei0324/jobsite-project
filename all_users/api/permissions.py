de9632bb7c4d06af4ed8645308ee11f8560f9082
fei0324 <fl7834a@student.american.edu>
2018-08-20 19:50:05 -0400

    added IsOwnerOrReadOnly permission

--- all_users/api/permissions.py
+++ all_users/api/permissions.py
@@ -5,3 +5,12 @@ class IsCandidate(permissions.BasePermission):
 	def has_permission(self, request, view):
 
 		return request.user.is_candidate
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+	def has_object_permission(self, request, view, obj):
+
+		if request.method in permissions.SAFE_METHODS:
+			return True
+
+		return obj.user.username == request.user.username

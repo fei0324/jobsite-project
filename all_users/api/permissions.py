b9946949ea4c229ca15e1b9826a9be7e015060cc
fei0324 <fl7834a@student.american.edu>
2018-08-22 15:35:45 -0400

    added IsOwner permission class

--- all_users/api/permissions.py
+++ all_users/api/permissions.py
@@ -14,3 +14,9 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 			return True
 
 		return obj.user.username == request.user.username
+
+class IsOwner(permissions.BasePermission):
+
+	def has_object_permission(self, request, view, obj):
+
+		return obj.username == request.user.username

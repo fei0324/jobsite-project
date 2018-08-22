915c92b17f734c8221b3698d85ac7bbe635bcb58
fei0324 <fl7834a@student.american.edu>
2018-08-22 15:34:38 -0400

    added all_users functional test - nonowner cannot view user detail

--- all_users/tests.py
+++ all_users/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, force_authenticate
 
-from .models import User
+
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 # Create your tests here.
 
@@ -27,3 +34,21 @@ class AllUserModelTest(TestCase):
 		employer_user = self.employer
 		self.assertEqual(employer_user.is_candidate, False)
 		self.assertEqual(employer_user.is_employer, True)
+
+class AllUserTests(APITestCase):
+
+	def setUp(self):
+
+		self.candidate = User.objects.create(
+			user_type = 'candidate',
+			username = 'first_candidate',
+		)
+		self.client.force_authenticate(self.candidate)
+
+	def test_nonowner_cannot_view_user_detail(self):
+
+		# This test is not working as expected
+		endpoint = reverse('user-api:detail', kwargs={'pk':2})
+		print(endpoint)
+		resp = self.client.get(endpoint)
+		self.assertEqual(resp.status_code, 403)

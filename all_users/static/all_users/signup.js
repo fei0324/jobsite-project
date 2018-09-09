5b7c8390e9cba479ebbc1d6df8c12f834ce592ac
fei0324 <fl7834a@student.american.edu>
2018-09-09 18:02:01 -0400

    added signup ajax, currently not working

--- all_users/static/all_users/signup.js
+++ all_users/static/all_users/signup.js
@@ -1,10 +1,34 @@
 // all_users signup.js
 $(function(){
+	console.log("Ready!")
+
+	var username = $("#signUpUsername").val();
+	var password1 = $("#signUpPassword1").val();
+	var password2 = $("#signUpPassword2").val();
+
+	var password = ""
+
+	if (password1 == password2){
+		password = password1
+	} else {
+		console.log("Passwords don't match.")
+	}
+
+	var user_type = $("#signUpUserType").val()
+
 	$("#signUpButton").submit(function(event){
 		$.ajax({
-			url: "api/user/create/"
-			}
-			console.log(url)
-		);
+			url: "api/user/create/",
+			data: {
+				username: username,
+				password: password,
+				user_type: user_type,
+			},
+			method: "POST",
+			success: function(url, data) {
+				console.log(url)
+				console.log(data)
+			},
+		});
 	});
 });
